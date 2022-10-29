e35b0be75d30b1f279681f9533bf16bb6126d08c
Christopher Lang <christopher.lang.256@gmail.com>
2022-10-29 22:09:55 +0100

    Fix symbolic link bugs

--- mypkg.c
+++ mypkg.c
@@ -6,6 +6,7 @@
 #include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <libgen.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,33 +31,19 @@ add_to_buffer(char *new, char *buf, size_t buf_size, int *buf_index)
     return 0;
 }
 
-char *
+int
 path_combine(char *dir, char *file, char *buf)
 {
     int i = 0;
-    if(add_to_buffer(dir, buf, PATH_MAX, &i)) {
-        errno = ENAMETOOLONG;
-        return NULL;
-    }
-
+    if(add_to_buffer(dir, buf, PATH_MAX - 1, &i))
+        return 1;
     if(i > 0 && buf[i - 1] != '/')
-        if(add_to_buffer("/", buf, PATH_MAX, &i)) {
-            errno = ENAMETOOLONG;
-            return NULL;
-        }
-
-    if(add_to_buffer(file, buf, PATH_MAX, &i)) {
-        errno = ENAMETOOLONG;
-        return NULL;
-    }
-
-    if(i >= PATH_MAX) {
-        errno = ENAMETOOLONG;
-        return NULL;
-    }
+        if(add_to_buffer("/", buf, PATH_MAX - 1, &i))
+            return 1;
+    if(add_to_buffer(file, buf, PATH_MAX - 1, &i))
+            return 1;
     buf[i] = '\0';
-
-    return buf;
+    return 0;
 }
 
 int
@@ -84,33 +71,69 @@ path_common_prefix(char *a, char *b)
     return ret;
 }
 
-char *
-path_relative(char *src, char *dst, char* buf)
+int
+path_relative(char *src_dir, char *dst_file, char* buf)
 {
-    int common_prefix = path_common_prefix(src, dst);
+    int ret = 0;
+    int common_prefix, buf_i, i;
+    char *real_src_dir, *real_dst_file;
+    real_src_dir = malloc(PATH_MAX);
+    real_dst_file = malloc(PATH_MAX);
+    if(real_src_dir == NULL || real_dst_file == NULL) {
+        perror("malloc failed");
+        ret = 1;
+        goto cleanup;
+    }
 
-    /* jump to after last common slash */
-    src += common_prefix + 1;
-    dst += common_prefix + 1;
+    if(realpath(src_dir, real_src_dir) == NULL) {
+        char *err = strerror(errno);
+        fprintf(stderr, "failed to get real path of '%s': %s\n", src_dir, err);
+        ret = 1;
+        goto cleanup;
+    }
+    if(realpath(dst_file, real_dst_file) == NULL) {
+        char *err = strerror(errno);
+        fprintf(stderr, "failed to get real path of '%s': %s\n", dst_file, err);
+        ret = 1;
+        goto cleanup;
+    }
 
-    int i = 0;
-    while(*src) {
-        if(*src == '/')
-            if(add_to_buffer("../", buf, PATH_MAX, &i)) {
+    /* add slash to end of real_src_dir */
+    i = strlen(real_src_dir);
+    if(i >= PATH_MAX - 1) {
+        fprintf(stderr, "path exceeds PATH_MAX '%s'\n", real_src_dir);
+        ret = 1;
+        goto cleanup;
+    }
+    real_src_dir[i] = '/';
+    real_src_dir[i + 1] = '\0';
+
+    common_prefix = path_common_prefix(real_src_dir, real_dst_file);
+
+    buf_i = 0;
+    i = common_prefix + 1;
+    while(real_src_dir[i]) {
+        if(real_src_dir[i] == '/')
+            if(add_to_buffer("../", buf, PATH_MAX - 1, &buf_i)) {
                 fprintf(stderr, "relative path name exceeds PATH_MAX\n");
-                return NULL;
+                ret = 1;
+                goto cleanup;
             }
-        src++;
+        i++;
     }
 
-    if(add_to_buffer(dst, buf, PATH_MAX, &i)
-            || i >= PATH_MAX) {
+    i = common_prefix + 1;
+    if(add_to_buffer(&real_dst_file[i], buf, PATH_MAX - 1, &buf_i)) {
         fprintf(stderr, "relative path name exceeds PATH_MAX\n");
-        return NULL;
+        ret = 1;
+        goto cleanup;
     }
+    buf[buf_i] = '\0';
 
-    buf[i] = '\0';
-    return buf;
+cleanup:
+    free(real_src_dir);
+    free(real_dst_file);
+    return ret;
 }
 
 int
@@ -118,7 +141,7 @@ touch_dir(char *dir)
 {
     /* check status of dir */
     struct stat dir_stat;
-    if(lstat(dir, &dir_stat)) {
+    if(stat(dir, &dir_stat)) {
         /* if dir does not exist make it */
         if(errno == ENOENT) {
             if(mkdir(dir, 0755)) {
@@ -132,38 +155,61 @@ touch_dir(char *dir)
         fprintf(stderr, "failed to stat file '%s' (%s)\n", dir, err);
         return 1;
     }
-
     /* if non-directory file exists with that name */
     if((dir_stat.st_mode & S_IFDIR) == 0) {
         fprintf(stderr, "file already exists at '%s'\n", dir);
         return 1;
     }
-
     /* if permissions of the dir are not 755 */
     if((dir_stat.st_mode & 0777) != 0755) {
         fprintf(stderr, "directory has invalid permissions '%s'\n", dir);
         return 1;
     }
-
     return 0;
 }
 
 int
-make_link(char *target, char *link_file)
+make_relative_link(char *target, char *link_file)
 {
-    if(symlink(target, link_file)) {
+    int ret = 0;
+    char *link_file_copy, *rel_path, *link_dir;
+    link_file_copy = malloc(PATH_MAX);
+    rel_path = malloc(PATH_MAX);
+    if(rel_path == NULL || link_file_copy == NULL) {
+        perror("malloc failed");
+        ret = 1;
+        goto cleanup;
+    }
+    if(strlen(link_file) >= PATH_MAX) {
+        fprintf(stderr, "file exceeds PATH_MAX '%s'\n", link_file);
+        ret = 1;
+        goto cleanup;
+    }
+    strcpy(link_file_copy, link_file);
+    link_dir = dirname(link_file_copy);
+    if(path_relative(link_dir, target, rel_path)) {
+        ret = 1;
+        goto cleanup;
+    }
+    if(symlink(rel_path, link_file)) {
         char *err = strerror(errno);
         fprintf(stderr,
             "failed to create symbolic link '%s' -> '%s' (%s)\n",
-            link_file, target, err);
-        return 1;
+            link_file, rel_path, err);
+        ret = 1;
+        goto cleanup;
     }
-    return 0;
+cleanup:
+    free(link_file_copy);
+    free(rel_path);
+    return ret;
 }
 
-int copy_link(char *src, char *dst)
+int
+copy_link(char *src, char *dst)
 {
-    int ret = 0, link_len;
+    int ret = 0;
+    int link_len;
     char *link = malloc(PATH_MAX);
     if(link == NULL) {
         perror("malloc failed\n");
@@ -190,28 +236,27 @@ cleanup:
 }
 
 int
-stow_real_dir(char *real_src_dir, char *real_dst_dir)
+stow_dir(char *src_dir, char *dst_dir)
 {
     int ret = 0;
-    char *real_src_file, *real_dst_file;
+    char *src_file, *dst_file;
     DIR *dir;
 
     /* open src dir */
-    dir = opendir(real_src_dir);
+    dir = opendir(src_dir);
     if(dir == NULL) {
         char *err = strerror(errno);
         fprintf(stderr,
-            "failed to open directory '%s' (%s)\n", real_src_dir, err);
+            "failed to open directory '%s' (%s)\n", src_dir, err);
         ret = 1;
         goto cleanup;
     }
 
-    /* allocate path strings */
-    real_src_file = malloc(PATH_MAX);
-    real_dst_file = malloc(PATH_MAX);
-    if(real_src_file == NULL || real_dst_dir == NULL) {
-        char *err = strerror(errno);
-        fprintf(stderr, "malloc failed (%s)\n", err);
+    /* allocate strings */
+    src_file = malloc(PATH_MAX);
+    dst_file = malloc(PATH_MAX);
+    if(src_file == NULL || dst_dir == NULL) {
+        perror("malloc failed");
         ret = 1;
         goto cleanup;
     }
@@ -223,25 +268,15 @@ stow_real_dir(char *real_src_dir, char *real_dst_dir)
             errno = 0;
             continue;
         }
-
-        /* get this files real src dir */
-        real_src_file = path_combine(real_src_dir, file->d_name, real_src_file);
-        if(real_src_file == NULL) {
-            char *err = strerror(errno);
-            fprintf(stderr,
-                "failed to get path of file in '%s' (%s)\n",
-                real_src_dir, err);
+        /* get src_file */
+        if(path_combine(src_dir, file->d_name, src_file)) {
+            fprintf(stderr, "path too long in '%s'\n", src_dir);
             ret = 1;
             goto cleanup;
         }
-
         /* get this files real dst dir */
-        real_dst_file = path_combine(real_dst_dir, file->d_name, real_dst_file);
-        if(real_dst_file == NULL) {
-            char *err = strerror(errno);
-            fprintf(stderr,
-                "failed to generate path of file in '%s' (%s)\n",
-                real_dst_dir, err);
+        if(path_combine(dst_dir, file->d_name, dst_file)) {
+            fprintf(stderr, "path too long in '%s'\n", dst_dir);
             ret = 1;
             goto cleanup;
         }
@@ -251,40 +286,29 @@ stow_real_dir(char *real_src_dir, char *real_dst_dir)
         } else if(file->d_type == DT_CHR) {
             printf("does not support character device files yet. skipping\n");
         } else if(file->d_type == DT_DIR) {
-            if(touch_dir(real_dst_file)) {
-                fprintf(stderr,
-                    "failed to create directory '%s'\n", real_dst_dir);
+            if(touch_dir(dst_file)) {
+                fprintf(stderr, "failed to create directory '%s'\n", dst_file);
                 ret = 1;
                 goto cleanup;
             }
-            if(stow_real_dir(real_src_file, real_dst_file)) {
+            if(stow_dir(src_file, dst_file)) {
                 ret = 1;
                 goto cleanup;
             }
         } else if(file->d_type == DT_FIFO) {
             printf("does not support fifo files yet. skipping\n");
         } else if(file->d_type == DT_LNK) {
-            if(copy_link(real_src_file, real_dst_file)) {
-                fprintf(stderr, "failed to copy link '%s'\n", real_src_file);
+            if(copy_link(src_file, dst_file)) {
+                fprintf(stderr, "failed to copy link '%s'\n", src_file);
                 ret = 1;
                 goto cleanup;
             }
         } else if(file->d_type == DT_REG) {
-            char *rel_path = malloc(PATH_MAX);
-            if(rel_path == NULL) {
-                fprintf(stderr, "failed to generate relative path\n");
+            if(make_relative_link(src_file, dst_file)) {
+                fprintf(stderr, "failed to link file '%s'\n", src_file);
                 ret = 1;
                 goto cleanup;
             }
-            rel_path = path_relative(real_dst_file, real_src_file, rel_path);
-            if(rel_path == NULL) {
-                fprintf(stderr, "failed to generate relative path\n");
-                ret = 1;
-                goto cleanup;
-            }
-            printf("%s -> %s\n", real_dst_file, real_src_file);
-            printf("%s\n", rel_path);
-            make_link(rel_path, real_dst_file);
         } else if(file->d_type == DT_SOCK) {
             printf("does not support socket files yet. skipping\n");
         } else if(file->d_type == DT_UNKNOWN) {
@@ -296,17 +320,18 @@ stow_real_dir(char *real_src_dir, char *real_dst_dir)
         }
         errno = 0;
     }
+
     if(errno != 0) {
         char *err = strerror(errno);
         fprintf(stderr,
-            "failed to read from directory '%s' (%s)\n", real_src_dir, err);
+            "failed to read from directory '%s' (%s)\n", src_dir, err);
         ret = 1;
         goto cleanup;
     }
 
 cleanup:
-    free(real_src_file);
-    free(real_dst_file);
+    free(src_file);
+    free(dst_file);
     closedir(dir);
     return ret;
 }
@@ -315,83 +340,29 @@ int
 install_pkg(char *pkg_dir, char *install_dir)
 {
     int ret = 0;
-    char *real_install_dir, *real_pkgfiles_dir, *old_working_dir;
+    char *pkgfiles_dir;
 
-    /* allocate memory for path strings */
-    real_install_dir = malloc(PATH_MAX);
-    real_pkgfiles_dir = malloc(PATH_MAX);
-    old_working_dir = malloc(PATH_MAX);
-    if(real_install_dir == NULL 
-            || real_pkgfiles_dir == NULL 
-            || old_working_dir == NULL) {
-        char *err = strerror(errno);
-        fprintf(stderr, "malloc failed (%s)\n", err);
+    pkgfiles_dir = malloc(PATH_MAX);
+    if(pkgfiles_dir == NULL) {
+        perror("malloc failed");
         ret = 1;
         goto cleanup;
-        return 1;
     }
 
-    /* get real_install_dir */
-    real_install_dir = realpath(install_dir, real_install_dir);
-    if(real_install_dir == NULL) {
-        char *err = strerror(errno);
+    if(path_combine(pkg_dir, PACKAGE_FILES_DIRNAME, pkgfiles_dir)) {
         fprintf(stderr,
-            "failed to get absolute path for '%s' (%s)\n", install_dir, err);
-        ret = 1;
-        goto cleanup;
-        return 1;
-    }
-
-    /* get current working directory */
-    old_working_dir = getcwd(old_working_dir, PATH_MAX);
-    if(old_working_dir == NULL) {
-        char *err = strerror(errno);
-        fprintf(stderr, "failed to get working directory (%s)\n", err);
+            "'%s' in '%s' exceeds PATH_MAX\n", PACKAGE_FILES_DIRNAME, pkg_dir);
         ret = 1;
         goto cleanup;
     }
-
-    /* move working directory into package */
-    if(chdir(pkg_dir)) {
-        char *err = strerror(errno);
-        fprintf(stderr,
-            "failed to enter package directory '%s' (%s)\n", pkg_dir, err);
-        ret = 1;
-        goto cleanup;
-    }
-
-    /* get real_pkg_dir */
-    real_pkgfiles_dir = realpath(PACKAGE_FILES_DIRNAME, real_pkgfiles_dir);
-    if(real_pkgfiles_dir == NULL) {
-        char *err = strerror(errno);
-        fprintf(stderr,
-            "failed to get absolute path for '%s' (%s)\n",
-            PACKAGE_FILES_DIRNAME, err);
-        ret = 1;
-        goto cleanup;
-    }
-
-    /* stow the directory */
-    if(stow_real_dir(real_pkgfiles_dir, real_install_dir)) {
-        fprintf(stderr, "failed to stow directory '%s'\n", real_pkgfiles_dir);
-        ret = 1;
-        goto cleanup;
-    }
-
-    /* return to old working directory */
-    if(chdir(old_working_dir)) {
-        char *err = strerror(errno);
-        fprintf(stderr,
-            "failed to return to working directory '%s' (%s)\n",
-            old_working_dir, err);
+    if(stow_dir(pkgfiles_dir, install_dir)) {
+        fprintf(stderr, "failed to stow directory '%s'\n", pkgfiles_dir);
         ret = 1;
         goto cleanup;
     }
 
 cleanup:
-    free(old_working_dir);
-    free(real_pkgfiles_dir);
-    free(real_install_dir);
+    free(pkgfiles_dir);
     return ret;
 }
 
